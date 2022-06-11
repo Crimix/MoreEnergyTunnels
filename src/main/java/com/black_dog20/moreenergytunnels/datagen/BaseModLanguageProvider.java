8367bd67198f7d5cf0fe9a7e2922b8f930775a1c
crimix <christofferm@live.dk>
2022-06-11 15:47:01 +0200

    Be able to support Compact Machines 4.2.0

--- src/main/java/com/black_dog20/moreenergytunnels/datagen/BaseModLanguageProvider.java
+++ src/main/java/com/black_dog20/moreenergytunnels/datagen/BaseModLanguageProvider.java
@@ -2,8 +2,8 @@ package com.black_dog20.moreenergytunnels.datagen;
 
 import com.black_dog20.bml.datagen.BaseLanguageProvider;
 import dev.compactmods.machines.api.tunnels.TunnelDefinition;
-import dev.compactmods.machines.i18n.TranslationUtil;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
 
 public abstract class BaseModLanguageProvider extends BaseLanguageProvider {
 
@@ -19,6 +19,7 @@ public abstract class BaseModLanguageProvider extends BaseLanguageProvider {
     }
 
     protected void addTunnel(TunnelDefinition tunnel, String name) {
-        add(TranslationUtil.tunnelId(tunnel.getRegistryName()), name);
+        ResourceLocation id = tunnel.getRegistryName();
+        add("item." + id.getNamespace() + ".tunnels." + id.getPath().replace('/', '.'), name);
     }
 }
